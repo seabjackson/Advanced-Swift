b8d082b20fad3362a63386c8afeb35ea6591d4ca
seabjnyc <honeyboyseab@gmail.com>
2017-04-23 14:00:35 -0400

    displayed how mutability of collections such as arrays work in Swift

--- Collections/Collections.playground/Contents.swift
+++ Collections/Collections.playground/Contents.swift
@@ -2,133 +2,43 @@
 
 import Cocoa
 
+/******************************************************************
+ *
+ * ADVANCED SWIFT
+ *
+ *
+ *****************************************************************/
 
 // Arrays and Mutability
 
+// The Fibonacci numbers
 let fibs = [0, 1, 1, 2, 3, 5]
-
 var mutableFibs = [0, 1, 1, 2, 3, 5]
 
 mutableFibs.append(8)
-mutableFibs.appendContentsOf([13, 21])
-
-// note in Swift that arrays are passed by value
+mutableFibs.append(contentsOf: [13, 21])
+mutableFibs
 
 var x = [1, 2, 3]
-
 var y = x
 y.append(4)
+y
+x
 
 let a = NSMutableArray(array: [1, 2, 3])
 
-// I don't want to be able to mutate b
-let b: NSArray = a.copy() as! NSArray
+// I don't want to be able to mutaye b
+let b: NSArray = a
+a.insert(4, at: 3)
+b
 
-a.insertObject(4, atIndex: 3)
-b // still is [1, 2, 3]
+let c = NSMutableArray(array: [1, 2, 3])
 
-// TRANSFORMING ARRAYS
-
-//  Map
-var squared: [Int] = []
-for fib in fibs {
-    squared.append(fib * fib)
-}
-
-let squares = fibs.map { fib in fib * fib }
-squared
-squares
-
-extension Array {
-    // Element is a generic placeholder for whatever type the array contains
-    func map<U>(transform: Element -> U) -> [U]  {
-        var result: [U] = []
-        result.reserveCapacity(self.count)
-        for x in self {
-            result.append(transform(x))
-        }
-        return result
-    }
-}
-
-
-// Mutation and Stateful Closures
-extension Array {
-    func accumulate<U>(initial: U, combine: (U, Element) -> U) -> [U] {
-        var running = initial
-        return self.map { next in
-            running = combine(running, next)
-            return running
-        }
-    }
-}
-
-[1, 2, 3, 4].accumulate(0, combine: +)
-
-// Filter
-fibs.filter { $0 % 2 == 0 }
-
-// Find all squares under 100 that are even
-print("\((1..<10).map {$0 * $0 }.filter {$0 % 2 == 0 })")
-
-// Reduce
-
-//var total = 0
-//for num in fibs {
-//    total = total + num
-//}
-
-fibs
-fibs.reduce(0, combine: +)
-
-// output of reduce does not have to be the same as the input
-fibs.reduce("") { str, num in str + "\(num)\n"}
-
-// A Flattening Map
-
-let suits = ["♤", "♡", "♧", "♢"]
-let ranks = ["J", "Q", "K", "A"]
-
-let allCombinations = suits.flatMap { suit in ranks.map { rank in  (suit, rank)} }
-print(allCombinations)
-
-// Iteration using for each
-
-for element in [1, 2, 3] {
-    print(element)
-}
-
-[1, 2, 3].forEach { element in
-    print(element)
-}
-
-// Slices
-
-fibs
-var slice = fibs[1..<fibs.endIndex] // gets me all but the first element in fibs
-slice // type is ArraySlice
-Array(slice)
-
-// Bridging
-
-var numbersArray = [1, 2, 3]
-let z: NSArray = numbersArray as NSArray
-z[0] is NSNumber
-
-
-
-// updated to swift 3 syntax
-
-
-
-
-
-
-
-
-
-
-
+// I don't want to be able to mutate d
+let d = c.copy() as! NSArray
+c.insert(4, at: 3)
+c
+d
 
 
 
